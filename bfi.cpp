17ce530ffc09f726c1e84a0c8df8b878ba5623d2
Ilia Shimozerov <ilia.shimozerov@gmail.com>
2023-11-13 01:36:11 +0300

    version 1.0

---
+++ bfi.cpp
@@ -0,0 +1,166 @@
+// BrainFuck Interpreter (BFI) v1.0
+
+#include <fstream>
+#include <iostream>
+
+constexpr int memSize = 65536;
+
+int memory[memSize];
+int pointer = 0;
+
+struct CharArray
+{
+	char* text;
+	int length;
+};
+
+/*Listing GetListingFromFile(char* fileName)
+{
+	std::ifstream inStream;
+	char op;
+	int i = 0;
+	inStream.open("source.bf");
+	while (inStream >> op) i++;
+	char* listing = new char[i];
+	i = 0;
+	while (inStream >> op) listing[i++] = op;
+	inStream.close();
+	std::cout << "Length: " << i << std::endl;
+	for (int j = 0; j < i; j++) std::cout << listing[j] << std::endl;
+	
+}*/
+
+void Interpret(char* listing, int length)
+{
+	int i = 0;
+	while (i < length)
+	{
+		char op = listing[i];
+		switch (op)
+		{
+		case 43:
+			memory[pointer]++;
+			i++;
+			break;
+		case 45:
+			memory[pointer]--;
+			i++;
+			break;
+		case 62:
+			pointer++;
+			if (pointer >= memSize) pointer -= memSize;
+			i++;
+			break;
+		case 60:
+			pointer--;
+			if (pointer < 0) pointer += memSize;
+			i++;
+			break;
+		case 46:
+			putchar(memory[pointer]);
+			i++;
+			break;
+		case 58:
+			std::cout << memory[pointer] << std::endl;
+			i++;
+			break;
+		case 44:
+			//std::cout << "Enter: ";
+		{
+			int c = 0;
+			std::cin >> c;
+			memory[pointer] = char(c);
+			//memory[pointer] = getchar();
+			i++;
+		}
+			break;
+		case 91:
+		{
+			int nested = 0;
+			int j = i;
+			int k = 0;
+			int p = pointer;
+			char* text = new char[length];
+			j++;
+			while (nested > -1)
+			{
+				if (listing[j] == 91) nested++;
+				if (listing[j] == 93) nested--;
+				text[k] = listing[j];
+				//for (int w = 0; w < k; w++) std::cout << text[w];
+				//std::cout << std::endl;
+				k++; j++;
+			}
+			/*while (listing[j] != 93)
+			{
+				text[k] = listing[j];
+				k++; j++;
+			}*/
+			//j++;
+			char* cycleBody = new char[k];
+			for (int l = 0; l < k - 1; l++) cycleBody[l] = text[l];
+			delete[] text;
+			//std::cout << "cycleBody: ";
+			//for (int w = 0; w < k-1; w++) std::cout << cycleBody[w];
+			//std::cout << std::endl;
+			while (memory[p] != 0) Interpret(cycleBody, k - 1);
+			i = j;
+		}	
+			break;
+
+		default:
+			std::cout << "Unexpected operator in " << i << " position!" << std::endl;
+			i++;
+			break;
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	char* text = new char[memSize];
+	if (argc != 1)
+	{
+		system("cls");
+		char* filename = argv[1];
+		int length = 0;
+		while (filename[length] != 0) length++;
+		int p = 0;
+		for (int i = 0; i < length; i++) if (filename[i] == 46) p = i;
+		if ((filename[p + 1] == 98 or filename[p + 1] == 66) and (filename[p + 2] == 102 or filename[p + 2] == 70))
+		{
+			std::ifstream inStream;
+			char op;
+			int i = 0;
+			inStream.open(filename);
+			char* text = new char[memSize];
+			while (inStream >> op) if (op != 10) text[i++] = op;
+			char* listing = new char[i];
+			for (int j = 0; j < i; j++) listing[j] = text[j];
+			inStream.close();
+			delete[] text;
+			std::cout << "BrainFuck Interpreter v1.0" << std::endl;
+			std::cout << "##########################\n" << std::endl;
+			std::cout << "File: " << filename << std::endl;
+			std::cout << "\nResult: ";
+			Interpret(listing, i);
+			std::cout << "\n##########################\n" << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "BrainFuck Interpreter v1.0" << std::endl;
+		std::cout << "##########################\n" << std::endl;
+		std::cout << ">>> ";
+		char op;
+		int length = 0;
+		std::cin >> text;
+		while (text[length] != 0) length++;
+		char* listing = new char[length];
+		for (int i = 0; i < length; i++) listing[i] = text[i];
+		delete[] text;
+		Interpret(listing, length);
+	}
+	system("pause");
+	system("cls");
+}
